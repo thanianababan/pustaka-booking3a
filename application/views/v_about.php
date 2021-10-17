f0a1f5a3b1e853a5c8f4a4aaedcae0b3645c2b51
thanianababan <thaniadwiaprilianababan@gmail.com>
2021-10-18 00:40:22 +0700

    commit

---
+++ application/views/v_about.php
@@ -0,0 +1,7 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <p>
+        Halaman About <br>
+        Nama : Thania Nababan <br>
+        Alamat : Tozai Baru, Kota Pematangsiantar
+    </p>
---
+++ application/views/v_index.php
@@ -0,0 +1,20 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi itaque ratione qui ipsam molestias esse temporibus ullam
+        repellendus labore impedit minima quasi, autem facilis fugit repudiandae quod? Dicta assumenda reiciendis ducimuscommodi
+        minima vero blanditiis doloribus praesentium necessitatibu. Hic dignissimos voluptatibus eum enim doloribus. Vel porro sunt
+        facilis perspiciatis distinctio!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore repellendus placeat, voluptatum ducimus quaerat, optio
+        magnam dolorem est in corporis minus, suscipit vel at voluptatibus.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia adipisci sit autem, voluptatem inventore repllat ipsam 
+        ducimus reiciendis iusto quibusdam totam eos magnam consequuntur modi, laborum laboriosam dolorem! Porro, amet.
+    </p>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam porro voluptatipus commodi voluptates maxiem, natus expedita
+        aut veniam. Quo quasi ipsum corrupti. Veniam eaque velit unde odio facilis ratione dolorem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat eius animi inventore similique nobis! Nihil expedita
+        magnam a, labore quas natus minus libero provident numquam repellat aspernatur amet sapiente facilis inventore nobis aliquid
+        porro modi beatae ipsam error voluptus odit, aerum quidem blanditiis! Eos sunt animi sapiente adipisci optio similique.  
+    </p>
+</section>
